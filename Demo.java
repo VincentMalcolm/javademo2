d2bff0c3acaa6a4d63468e4020cd889369abe0d2
VincentMalcolm <vincentmalcolm9225@gmail.com>
2026-02-16 21:01:56 +0000

    program asks for secret code

--- Demo.java
+++ Demo.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class Demo
   {
     public static void main(String[] args)
     {
-      System.out.println("It works!");
+      Scanner keyboard = new Scanner(System.in);
+      
+      System.out.println("Enter secret code:   ");
+      String code = keyboard.nextLine();
+      if(code.equals("tacoman")) // this is the .equals method. used when dealing with strings instead of using ==
+      {
+        System.out.println("Access Granted");
+      }
+      else
+      {
+        System.out.println("Access Denied");
+      }
     }
   }
     
